d7e5074bca930da3d4442ff91de8a11543e75566
Sam - Linux PC <samuel.dacosta-2@student.manchester.ac.uk>
2020-04-01 21:26:01 +0100

    Packages managed

--- src/maze/RouteFinder.java
+++ src/maze/RouteFinder.java
@@ -1,10 +1,24 @@
-package maze;
+package maze.routing;
 import java.util.Stack;
+import maze.*;
+import java.util.List;
 public class RouteFinder{
   private Maze maze;
   private Stack<Tile> route;
   private boolean finished;
   public RouteFinder(Maze maze){
     this.maze = maze;
+  }
+  public Maze getMaze(){
+    return this.maze;
+  }
+  public List<Tile> getRoute(){
+    ;
+  }
+  public boolean isFinished(){
+    return this.finished;
+  }
+  public static RouteFinder load(String path){
+    ;
   }
 }
