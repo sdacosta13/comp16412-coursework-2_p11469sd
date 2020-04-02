98a61573989a5f854c843d7d089e9d8c7d6112f5
Sam - Linux PC <samuel.dacosta-2@student.manchester.ac.uk>
2020-04-02 20:51:46 +0100

    rendering reworked

--- src/maze/visualisation/LoadMazeHandle.java
+++ src/maze/visualisation/LoadMazeHandle.java
@@ -8,22 +8,43 @@ import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.event.*;
 import maze.*;
+import maze.routing.RouteFinder;
 import java.io.File;
+import java.util.ArrayList;
 import javafx.scene.input.MouseEvent;
 
-public class LoadMazeHandle implements EventHandler<ActionEvent>{
+
+
+public class LoadMazeHandle implements EventHandler<MouseEvent>{
   public Stage stage;
-  public String path;
-  public LoadMazeHandle(Stage s){
-    this.stage = s;
+  public Group buttons;
+  public MazeApplication mApp;
+  public LoadMazeHandle(Stage stage, Group buttons, MazeApplication mApp){
+    this.stage = stage;
+    this.buttons = buttons;
+    this.mApp = mApp;
   }
   @Override
-  public void handle(ActionEvent event){
+  public void handle(MouseEvent event){
+    this.buttons = this.mApp.setupButtons();
     FileChooser fl = new FileChooser();
-    File selected = fl.showOpenDialog(this.stage);
+    FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("TEXT files (*.txt)","*.txt");
+    fl.getExtensionFilters().add(filter);
+    File selected = null;
+    selected = fl.showOpenDialog(this.stage);
     if(selected != null){
-      this.path = selected.getAbsolutePath();
+      String path = selected.getAbsolutePath();
+      try{
+        Maze m = Maze.fromTxt(path);
+        RouteFinder newRouteFinder = new RouteFinder(m);
+        this.mApp.setRouteFinder(newRouteFinder);
+        ArrayList<Rectangle> rects = this.mApp.printRouteFinder(newRouteFinder.toString());
+        this.buttons.getChildren().addAll(rects);
+        this.buttons.getChildren().get(0).setVisible(true);
+        this.mApp.changeScene(this.buttons);
+      } catch (InvalidMazeException e){
+        System.out.println("Something went wrong loading " + path);
+      }
     }
   }
-
 }
--- src/maze/visualisation/MazeApplication.java
+++ src/maze/visualisation/MazeApplication.java
@@ -7,6 +7,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.event.*;
+import javafx.scene.input.MouseEvent;
 import java.io.File;
 import java.util.ArrayList;
 import maze.*;
@@ -19,97 +20,50 @@ public class MazeApplication extends Application{
   private RouteFinder route;
   public void start(Stage stage) throws InvalidMazeException{
     this.stage = stage;
-    this.setupButtons();
-    Scene scene = new Scene(this.objs, 600,800);
-    this.stage.setScene(scene);
-    this.stage.setTitle("Maze Solver");
+    Group newButtons = this.setupButtons();
+    Scene newScene = new Scene(newButtons, 800,600);
+    this.stage.setScene(newScene);
     this.stage.show();
-
+  }
+  public void changeScene(Group newObjs){
+    Group tempGroup = new Group();
+    tempGroup.getChildren().addAll(newObjs.getChildren());
+    this.stage.hide();
+    Scene newScene = new Scene(tempGroup);
+    this.stage.setScene(newScene);
+    this.stage.show();
   }
   public Group getBaseGroup(){
+    return null;
+  }
+
+  public Group setupButtons(){
     Button load = new Button("Load Maze");
-    load.setPrefSize(90,30);
+    Button loadR = new Button("Load Route");
+    Button saveR = new Button("Save Route");
+    load.setPrefSize(100,30);
     load.setLayoutX(0);
     load.setLayoutY(0);
 
-    load.setOnAction((e) -> {
-      FileChooser fl = new FileChooser();
-      FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("TEXT files (*.txt)","*.txt");
-      fl.getExtensionFilters().add(filter);
-      File selected = null;
-      selected = fl.showOpenDialog(this.stage);
-      if(selected != null){
-        String path = selected.getAbsolutePath();
-        Group newGroup = this.getBaseGroup();
-        try{
-          Maze m = Maze.fromTxt(path);
-          this.setRouteFinder(new RouteFinder(m));
-          int yOffset = 90;
-
-          this.stage.hide();
-          newGroup.getChildren().addAll(objs.getChildren());
-
-          String stringMaze = this.getRouteFinder().toString();
-          ArrayList rects = this.printRouteFinder(stringMaze, yOffset);
-          for(int i = 0; i < rects.size(); i++){
-            Rectangle newRect = (Rectangle) rects.get(i);
-            newGroup.getChildren().add(newRect);
-          }
-
-
-
-          Button step = new Button("Step");
-          step.setPrefSize(90,30);
-          step.setLayoutX(0);
-          step.setLayoutY(yOffset + (m.getDimensions()[1]*this.cellSize));
-
-          step.setOnAction((f) -> {
-            try{
-              Group newGroup2 = this.getBaseGroup();
-              this.stage.hide();
-              this.getRouteFinder().step();
-              String stringMaze2 = this.getRouteFinder().toString();
-              ArrayList<Rectangle> rects2 = this.printRouteFinder(stringMaze, yOffset);
-              for(int i = 0; i < rects.size(); i++){
-                Rectangle newRect = (Rectangle) rects.get(i);
-                newGroup.getChildren().add(newRect);
-              }
-              Scene scene = new Scene(newGroup);
-              this.stage.setScene(scene);
-              this.stage.show();
-
-
-            } catch(NoRouteFoundException noRoute) {
-              System.out.println("No Route found");
-            }
-
-          });
-          newGroup.getChildren().add(step);
-          Scene scene = new Scene(newGroup);
-          this.stage.setScene(scene);
-          this.stage.show();
-
-        } catch (InvalidMazeException newException){
-          System.out.println("Error Loading maze");
-        }
-      }
-    });
-
-    Button loadR = new Button("Load Route");
     loadR.setPrefSize(100,30);
     loadR.setLayoutX(0);
     loadR.setLayoutY(30);
-    loadR.setOnAction((e)-> {
-
-    });
-    Button saveR = new Button("Save Route");
     saveR.setPrefSize(100,30);
     saveR.setLayoutX(0);
     saveR.setLayoutY(60);
-    Group newGroup = new Group(load, loadR, saveR);
-    return newGroup;
+
+    Button step = new Button("Step");
+    step.setPrefSize(100,30);
+    step.setLayoutX(0);
+    step.setLayoutY(90);
+    step.setVisible(false);
+
+    Group buttons = new Group(step, load, loadR, saveR);
+    load.addEventHandler(MouseEvent.MOUSE_CLICKED, new LoadMazeHandle(this.stage, buttons, this));
+    return buttons;
   }
-  public ArrayList<Rectangle> printRouteFinder(String stringMaze,int yOffset){
+  public ArrayList<Rectangle> printRouteFinder(String stringMaze){
+    int yOffset = 120;
     ArrayList<Rectangle> rects = new ArrayList<Rectangle>();
     int x = 0;
     int y = 0;
@@ -135,18 +89,6 @@ public class MazeApplication extends Application{
       }
     }
     return rects;
-  }
-  public void setupButtons() throws InvalidMazeException{
-    this.objs = getBaseGroup();
-
-  }
-  public void loadMaze(String path) throws InvalidMazeException{
-
-    Scene scene = new Scene(this.objs, 600,800);
-    this.stage.setScene(scene);
-    this.stage.setTitle("Maze Solver");
-    this.stage.show();
-
   }
 
   public RouteFinder getRouteFinder(){
